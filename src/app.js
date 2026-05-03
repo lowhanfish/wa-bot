d2cbd9c5f0a100605a9971af1025b811426f58e4
lowhanfish <lowhanfish@gmail.com>
2026-05-03 14:18:13 +0800

    restructure

--- src/app.js
+++ src/app.js
@@ -3,14 +3,13 @@ import 'dotenv/config'
 import express from 'express'
 
 
-const app = express()
+const app = express();
+app.use(express.json());
 const port = process.env.PORT
 
 app.get("/", (req, res)=>{
     res.send(`Server Active on Port : ${port}`)
 })
-
-
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
